d8bae2d40b77e2fffde4c4db3d4095b13e14af21
Noelia Donato <noeliadonato@gmail.com>
2022-06-01 20:26:16 -0300

    feat(NavBar): add navbar to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
 
   return (
     <div className="App">
       {/* Aca vamos a ir importando los componentes que vayamos haciendo, segun corresponda */}
+      <NavBar />
     </div>
   )
 }
