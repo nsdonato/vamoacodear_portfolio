54e47655842be572db2d6b427a7968ba4c21b10f
juliantrovato <julian_todobien@hotmail.com>
2022-06-01 19:31:28 -0300

    algunos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { ContactForm } from './components/ContactForm/ContactForm'
+import { ContatcMe } from './components/ContactMe/ContactMe'
 import logo from './logo.svg'
 import './App.css'
 
@@ -7,37 +9,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <ContatcMe />
+      <ContactForm />
     </div>
   )
 }
---
+++ src/components/ContactMe/ContactMe.jsx
@@ -0,0 +1,10 @@
+export const ContatcMe = ()=>{
+    return (
+        <section id="contact">
+            <div className="contact_header">
+            <h1 className="contact_title">CONTACT ME</h1>
+            <div className="contact_guion"></div>
+            </div>
+        </section>
+    )
+}
