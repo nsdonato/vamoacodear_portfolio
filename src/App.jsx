ef00ea8af0aceba9db07583dcc541a1a29f35912
Your Name <you@example.com>
2022-06-02 00:24:47 -0300

    feat(Projects): import ProjectItem

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <Projects/> 
       <About />
-      <Projects/>
       <ContatcMe />
     </div>
   )
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,3 +1,4 @@
+import ProjectItem from "../NavBarItem/NavBarItem"
 import "./Projects.css"
 
 export const Projects = () =>{
@@ -7,7 +8,12 @@ export const Projects = () =>{
             <div class="portfolio_guion"></div>
               <div class="portfolio_flex_content">
               
-               Proyectos
+               <ProjectItem className="portfolio_flex_item portfolio_cabin" src="bkp/image/cabin.png" alt="Imagen de una cabina" ></ProjectItem>
+               <ProjectItem className="portfolio_flex_item portfolio_cake w_33" src="bkp/image/cake.png" alt="Imagen de una torta" ></ProjectItem>
+               <ProjectItem className="portfolio_flex_item portfolio_circus w_33" src="bkp/image/circus.png" alt="Imagen de un circo" ></ProjectItem>
+               <ProjectItem className="portfolio_flex_item portfolio_game w_33" src="bkp/image/game.png" alt="Imagen de un joystick" ></ProjectItem>
+               <ProjectItem className="portfolio_flex_item portfolio_safe w_33" src="bkp/image/safe.png" alt="Imagen de una caja fuerte" ></ProjectItem>
+               <ProjectItem className="portfolio_flex_item portfolio_submarine w_33" src="bkp/image/submarine.png" alt="Imagen de un submarino" ></ProjectItem>
                 
             </div>
     </section>
