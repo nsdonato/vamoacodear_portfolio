8b14dbece09c235324797d596dbcc2bfebe1faa1
Noelia Donato <noeliadonato@gmail.com>
2022-06-02 19:52:31 -0300

    fix images

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,12 +7,12 @@ export const Projects = () => {
       <h1 className="portfolio_title">PORTFOLIO</h1>
       <div className="portfolio_guion"></div>
       <div className="portfolio_flex_content">
-        <ProjectItem className="portfolio_cabin" src="bkp/image/cabin.png" alt="Imagen de una cabina" />
-        <ProjectItem className="portfolio_cake w_33" src="bkp/image/cake.png" alt="Imagen de una torta" />
-        <ProjectItem className="portfolio_circus w_33" src="bkp/image/circus.png" alt="Imagen de un circo" />
-        <ProjectItem className="portfolio_game w_33" src="bkp/image/game.png" alt="Imagen de un joystick" />
-        <ProjectItem className="portfolio_safe w_33" src="bkp/image/safe.png" alt="Imagen de una caja fuerte" />
-        <ProjectItem className="portfolio_submarine w_33" src="bkp/image/submarine.png" alt="Imagen de un submarino" />
+        <ProjectItem className="portfolio_cabin" src="/img/cabin.png" alt="Imagen de una cabina" />
+        <ProjectItem className="portfolio_cake w_33" src="/img/cake.png" alt="Imagen de una torta" />
+        <ProjectItem className="portfolio_circus w_33" src="/img/circus.png" alt="Imagen de un circo" />
+        <ProjectItem className="portfolio_game w_33" src="/img/game.png" alt="Imagen de un joystick" />
+        <ProjectItem className="portfolio_safe w_33" src="/img/safe.png" alt="Imagen de una caja fuerte" />
+        <ProjectItem className="portfolio_submarine w_33" src="/img/submarine.png" alt="Imagen de un submarino" />
       </div>
     </section>
   )
